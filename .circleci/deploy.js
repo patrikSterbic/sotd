2c6c6b1a96ff08e1dd48be23cb04f8ee67dfc455
Sterba Patrik <sterba@cngroup.dk>
2019-05-29 12:07:37 +0200

    Oprava FTP

--- .circleci/deploy.js
+++ .circleci/deploy.js
@@ -2,7 +2,7 @@ const FtpDeploy = require("ftp-deploy");
 const ftpDeploy = new FtpDeploy();
 
 const config = {
-  username: process.env.FTPUSERNAME,
+  user: process.env.FTPUSERNAME,
   password: process.env.FTPPASS,
   host: process.env.FTPHOST,
   port: 21,
